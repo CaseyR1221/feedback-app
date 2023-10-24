bb247b1870d8234eeb95679ceb377b8f1187090c
Casey R <crowlandsdev@gmail.com>
2023-10-24 09:31:04 -0400

    added form for ratings and added message length conditions for rating form

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,46 @@
+import Card from "./shared/Card"
+import { useState } from "react"
+import Button from "./shared/Button"
+
+const FeedbackForm = () => {
+  const [text, setText] = useState('')
+  const [btnDisabled, setbtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
+
+  const handleTextChange = (e) => {
+    if (text === '') {
+      setbtnDisabled(true)
+      setMessage(null)
+    } else if (text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be at least 10 characters')
+      setbtnDisabled(true)
+    } else {
+      setMessage(null)
+      setbtnDisabled(false)
+    }
+
+    setText(e.target.value)
+  }
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        {/* @todo - rating select component */}
+        <div className="input-group">
+          <input 
+            type="text" 
+            placeholder="Write a review" 
+            onChange={handleTextChange} 
+            value={text} 
+          />
+          <Button type="submit" isDisabled={btnDisabled}>Send</Button>
+        </div>
+
+        {message && <div className="message">{message}</div>}
+      </form>
+    </Card>
+  )
+}
+
+export default FeedbackForm
